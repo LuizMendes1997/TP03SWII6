cf5a7f476ab756a9cb4e2fc988a6ffa18d623bd2
Luiz Mendes <120228527+LuizMendes1997@users.noreply.github.com>
2024-11-11 20:14:40 -0300

    Add files via upload

---
+++ Data/SistemaTP03Context.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SistemaTP03.Models;
+
+namespace SistemaTP03.Data
+{
+    public class SistemaTP03Context : DbContext
+    {
+        public SistemaTP03Context (DbContextOptions<SistemaTP03Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SistemaTP03.Models.Produto> Produto { get; set; } = default!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+            modelBuilder.Entity<Produto>()
+                .Property(p => p.Preco)
+                .HasPrecision(18, 2);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
---
+++ Models/Produto.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SistemaTP03.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O nome do produto é obrigatório.")]
+        [StringLength(100, ErrorMessage = "O nome do produto deve ter no máximo 100 caracteres.")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "A descrição do produto é obrigatória.")]
+        [StringLength(500, ErrorMessage = "A descrição do produto deve ter no máximo 500 caracteres.")]
+        public string Descricao { get; set; }
+
+        [Required(ErrorMessage = "O preço do produto é obrigatório.")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "O preço deve ser maior que zero.")]
+        public decimal Preco { get; set; }
+
+        [Required(ErrorMessage = "A quantidade em estoque é obrigatória.")]
+        [Range(0, int.MaxValue, ErrorMessage = "A quantidade em estoque não pode ser negativa.")]
+        public int QuantidadeEmEstoque { get; set; }
+    }
+}
